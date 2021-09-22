05427b431035f2f5984d7de05ea236d0af54c1a7
Bilal-yat <59781894+Bilal-yat@users.noreply.github.com>
2021-09-22 19:38:09 -0400

    Update Inpatient.cpp

--- Hospital Management System/Inpatient.cpp
+++ Hospital Management System/Inpatient.cpp
@@ -1,14 +1,3 @@
-//
-//  Inpatient.cpp
-//  Assignment 4
-//
-//  Created by Bilal Yattou on 2020-03-30.
-//  Copyright © 2020 Bilal Yattou. All rights reserved.
-//
-// Name: Bilal Yattou  ID: 40110820
-// Partner: Matthieu Pourrat ID:40089209
-// IDE: Xcode
-
 #include <stdio.h>
 #include "Inpatient.h"
 #include <string>
