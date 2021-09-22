06becab053689a5185acd2840e838f407a06f631
Bilal-yat <59781894+Bilal-yat@users.noreply.github.com>
2021-09-22 19:37:13 -0400

    Update Hospital.cpp

--- Hospital Management System/Hospital.cpp
+++ Hospital Management System/Hospital.cpp
@@ -1,14 +1,3 @@
-//
-//  Hospital.cpp
-//  Assignment 4
-//
-//  Created by Bilal Yattou on 2020-03-30.
-//  Copyright © 2020 Bilal Yattou. All rights reserved.
-//
-// Name: Bilal Yattou  ID: 40110820
-// Partner: Matthieu Pourrat ID:40089209
-// IDE: Xcode
-
 #include <stdio.h>
 #include "Hospital.h"
 using namespace std;
