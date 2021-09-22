d159ddfde3780db656615822832c89b4bdb32396
Bilal-yat <59781894+Bilal-yat@users.noreply.github.com>
2021-09-22 19:37:39 -0400

    Update Hospital.h

--- Hospital Management System/Hospital.h
+++ Hospital Management System/Hospital.h
@@ -1,14 +1,3 @@
-//
-//  Hospital.h
-//  Assignment 4
-//
-//  Created by Bilal Yattou on 2020-03-30.
-//  Copyright © 2020 Bilal Yattou. All rights reserved.
-//
-// Name: Bilal Yattou  ID: 40110820
-// Partner: Matthieu Pourrat ID:40089209
-// IDE: Xcode
-
 #include <iostream>
 #include "Bed.h"
 #include "Inpatient.h"
