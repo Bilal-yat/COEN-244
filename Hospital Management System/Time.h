cec8fde0120243f76351de3175aaf6a328268c2e
Bilal-yat <59781894+Bilal-yat@users.noreply.github.com>
2021-09-22 19:40:25 -0400

    Update Time.h

--- Hospital Management System/Time.h
+++ Hospital Management System/Time.h
@@ -1,14 +1,3 @@
-//
-//  Time.h
-//  Assignment 4
-//
-//  Created by Bilal Yattou on 2020-03-29.
-//  Copyright © 2020 Bilal Yattou. All rights reserved.
-//
-// Name: Bilal Yattou  ID: 40110820
-// Partner: Matthieu Pourrat ID:40089209
-// IDE: Xcode
-
 #include <iostream>
 #pragma once
 
